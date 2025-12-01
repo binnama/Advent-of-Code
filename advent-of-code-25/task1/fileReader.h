7c53f152cd2b0fa9c2fcd68cf61c91e681db6bb1
binnama <laasestad@yahoo.no>
2025-12-01 21:46:51 +0100

    Small changes to fileReader

--- advent-of-code-25/task1/fileReader.h
+++ advent-of-code-25/task1/fileReader.h
@@ -3,7 +3,16 @@
 #ifndef FILEREADER_H
 #define FILEREADER_H
 
-string readFile(const string& filePath);
+namespace 
+{
+    public:
+        int readFile(const string& filePath);
+
+    private:
+        string line_;
+        int currentNumber_;
+        int zeroCount_;
+}
 
 
 #endif // FILEREADER_H
